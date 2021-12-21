a885c7318cab7a2d8888de9dc65267d50818d080
Vitor <costec222@gmail.com>
2021-12-21 18:00:17 -0300

    ajuste de calculo de data

--- Program.cs
+++ Program.cs
@@ -2,9 +2,24 @@
 public class Program { 
 
     public static double capital,taxa,tipoJuros,resultado;
-    public static DateTime resgate;
+    public static string data_fin;
+    public static int tempo;
     public static void Main(){
-      
+       
+        Console.WriteLine("Digite o Capital Inicial: ");
+        capital= double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a taxa de Juros: ");
+        taxa = double.Parse(Console.ReadLine())/100;
+        Console.WriteLine("Digite a data do resgate: ");
+        data_fin = Console.ReadLine();
+
+        var resgate = DateTime.Parse(data_fin);
+        var data_ini = DateTime.Now;
+        tempo= ajustaMesAno(resgate) - ajustaMesAno(data_ini);
+        
+        
+      // TimeSpan tempos= resgate.Substract(DateTime.Now.Month);
+
         Console.WriteLine("Escolha a Modalidade de Juros [0]Simples ou [1] Composto");
         tipoJuros = double.Parse(Console.ReadLine());
         
@@ -13,11 +28,11 @@ public class Program {
         switch (tipoJuros){
 
             case 0: Console.WriteLine("Modalidade Escolhida: Juros Simples");
-            tipoJuros = Simples(tipoJuros, capital, taxa, resgate);
+            tipoJuros = Simples(tipoJuros, capital, taxa);
             break;
 
             case 1: Console.WriteLine("Modalidade Escolhida: Juros Compostos");
-            tipoJuros = Composto(tipoJuros, capital, taxa, resgate);
+            tipoJuros = Composto(tipoJuros, capital, taxa);
             break;
 
             default: Console.WriteLine("Opção Inválida");
@@ -26,36 +41,37 @@ public class Program {
     }
 }
 
-public static double Simples(double tipoJuros, double capital, double taxa, DateTime resgate){
+public static double Simples(double tipoJuros, double capital, double taxa){
 
-Console.WriteLine("Digite o Capital Inicial: ");
-capital= double.Parse(Console.ReadLine());
-Console.WriteLine("Digite a taxa de Juros: ");
-taxa = double.Parse(Console.ReadLine())/100;
-Console.WriteLine("Digite a data do resgate: ");
-resgate =new DateTime(Console.Read());
-int tempo= resgate.Month - DateTime.Now.Month;
 double resultado = capital * tempo * taxa;
-Console.WriteLine("Seu resgate ocorrerá em: {0} meses",tempo);
-return resultado;
-
-}
-
-public static double Composto(double tipoJuros, double capital, double taxa, DateTime resgate){
-
-Console.WriteLine("Digite o Capital Inicial: ");
-capital= double.Parse(Console.ReadLine());
-Console.WriteLine("Digite a taxa de Juros: ");
-taxa = double.Parse(Console.ReadLine())/100;
-Console.WriteLine("Digite a data do resgate: ");
-resgate =new DateTime(Console.Read());
-int tempo= resgate.Month - DateTime.Now.Month;   
-double resultado = capital * Math.Pow((1+ taxa), tempo);
 Console.WriteLine("Seu resgate ocorrerá em: {0} meses",tempo);
 Console.WriteLine("O valor final é: {0}",resultado);
 return resultado;
 
 }
+
+public static double Composto(double tipoJuros, double capital, double taxa){
+/*var resgate = DateTime.Parse(data_fin);
+var data_ini = DateTime.Now;
+var tempo= (data_ini.Date.Month - resgate.Date.Month);*/ 
+double resultado = capital * Math.Pow((1+ taxa), (tempo));
+Console.WriteLine("Seu resgate ocorrerá em: {0} meses",tempo);
+Console.WriteLine("O valor final é: {0}",resultado);
+return resultado;
+
+}
+
+static int ajustaMesAno(DateTime d) {
+    return d.Year * 12 + d.Month;
+}
+/*
+public static int tempo(string data_fin ){
+
+    var resgate = DateTime.Parse(data_fin);
+        var data_ini = DateTime.Now;
+        var tempo= (resgate.Date.Month - data_ini.Date.Month);
+        return tempo;
+}*/
 
 }
 
